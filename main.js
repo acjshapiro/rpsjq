1ff04c3e843ff91ce985e32b00d336723264610e
Alex Shapiro <acjshapiro@gmail.com>
2018-02-14 16:42:33 -0700

    grab ID onclick assign to userchoice variable

--- main.js
+++ main.js
@@ -1,50 +1,30 @@
-// var = computerChoice
-// var = userChoice
-// var = result
-//
-// function(){
-//   //gets user choice from click
-//
-//
 
 $(document).ready( function() {
-
+var uChoice
 var cChoiceArray = ['rock', 'paper', 'scissor']
 var result
 var cChoice = cChoiceArray[Math.floor(Math.random()* 3)]
-var uChoice =
 
 $('.choice').on('click', function(){
-  alert('Button clicked')
-            
+  var uChoice = this.id
+
 })
 
 $('.choice').on('click', startGame)
 
 
-// function computerChoice(){
-//   var compChoice = cChoiceArray[Math.floor(Math.random()* 3)]
-//   console.log(cChoice)
-
-
-// function startGame(e) {             //start game function(event)
-//   var choice = e.target.id     //assign the user choice from target.id to local variable choice
-//   uChoice = choice             //assign choice to uChoice ^ from top. reassigns from null
-//   console.log(choice)          // prints choice to console for testing
-//   computerChoice()            //runs function computerChoice()
-//   console.log(compare())
-//   //results()    //will print the results of my compare function below to console for testing
-// }
-
 function startGame(){
+  var uChoice = this.id
   console.log(uChoice)
   console.log(cChoice)
   compare()
+  console.log(compare())
   printResults()
 }
 
 
 function compare(){
+  var uChoice = this.id
   if (uChoice == cChoice){
     return "Looks like a tie"
     result == "Tie";
