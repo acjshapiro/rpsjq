0fafec59e816561f491bf73d8bf739248f15603d
Alex Shapiro <acjshapiro@gmail.com>
2018-02-15 18:19:56 -0700

    comments cleanup switch to main2

---
+++ main2.js
@@ -0,0 +1,51 @@
+var result  //Declare my result variable OUTSIDE OF THE main $(document)...{}
+var cChoice //Declare my computer choice variable OUTSIDE OF THE main $(document)...{}
+var uChoice //Declare my User Choice Variable OUTSIDE OF THE main $(document)...{}
+var cChoiceArray = ['rock', 'paper', 'scissor'] //Creat array for cChoice
+
+$(document).ready( function() {  //Makse sure everything is loaded before running
+                                //Wrap all further code inside this bracket
+
+//When user clicks an object with class: choice (all images) run startGame function
+$('.choice').on('click', startGame)
+
+//Function for getting the computer Choice
+// access my array and the index of [0,1,2] chosen randomly
+//and assign it to the cChoice varriable
+function getCompChoice(){
+   cChoice = cChoiceArray[Math.floor(Math.random()* 3)]
+}
+
+//Start the Game! called by user clicking an image
+
+function startGame(){
+  getCompChoice() //Run my computer choice function
+  uChoice = this.id //"Grabb the id of the object "This" -being the clicked image- "
+  console.log(uChoice) //can see in the console if the choices were done correctly
+  console.log(cChoice)
+  compare()  //Run the compare function below to see who won
+  $('#uchoice').text('You Chose: ' + uChoice) //Inject user choice into HTML with the ID: choice (blank h2)
+  $('#cchoice').text('I Chose: ' + cChoice) //Inject computer choice into HTML with the ID: cChoice
+  $('#results').text(result) //Replace the text of the HTML with the ID: results with my results variable
+                              //which will be defined below in the compare function
+}
+//Compare function logic
+  function compare(){
+
+    if (uChoice == cChoice){
+      result = "Tie"  //assign this string to result variable so I can plug into HTML
+    }else if(uChoice == "rock" && cChoice == "paper"){
+        result = "You Lose"
+    }else if (uChoice == "rock" && cChoice == "scissor"){
+        result = "You Win"
+    }else if (uChoice == "paper" && cChoice == "rock" ){
+        result = "You Win"
+    }else if (uChoice == "paper" && cChoice == "scissor" ){
+        result = "You Lose"
+    }else if (uChoice == "scissor" && cChoice == "paper" ){
+        result = "You Win"
+    }else if (uChoice == "scissor" && cChoice == "rock"){
+      result = "You Lose"
+    }
+  }
+})
